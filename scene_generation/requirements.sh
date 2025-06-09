703d3b6852501b8fa709d4461c92303afe442028
abhilashreddys <sareddy53@gmail.com>
2025-06-09 07:01:25 +0000

    requirements update cuda12.4

--- scene_generation/requirements.sh
+++ scene_generation/requirements.sh
@@ -1,8 +1,8 @@
 # yes | conda create -n cse252d python=3.10
 # conda activate cse252d
 pip install uv
-uv pip install torch torchvision torchaudio
+uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
 uv pip install -U "autogen-agentchat" "autogen-ext[openai]"
 uv pip install transformers datasets diffusers peft accelerate crewai ipykernel sentencepiece gradio
 pip install qwen-vl-utils
-# uv pip install flash-attn --no-build-isolation
+uv pip install flash-attn --no-build-isolation
